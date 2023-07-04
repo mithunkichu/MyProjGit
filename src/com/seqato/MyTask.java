bec82719e681ec9c0ede2223a4e05dc9a724cea1
mithunkichu <mithunkrishna657@gmail.com>
2023-07-04 10:02:28 +0530

    First commit to master

---
+++ src/com/seqato/MyTask.java
@@ -0,0 +1,10 @@
+package com.seqato;
+
+public class MyTask {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author seq_mithun
+ *
+ */
+module ExampleGit {
+}
